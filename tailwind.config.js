94e8abf60ac89332fc9fe9fe73a3348057cfe463
Amit Tambulkar <amittambulkar104@gmail.com>
2024-03-02 14:55:29 +0530

    NFT card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  safelist: ["max-w-[90%]", "sm:max-w-[30rem]"],
   theme: {
     extend: {},
   },
